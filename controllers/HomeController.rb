fdf9f47e5ffddb6363f922cba37529ad83378073
Tim Rourke <tim@timrourke.com>
2015-08-19 11:32:20 -0500

    adding example 3

--- controllers/HomeController.rb
+++ controllers/HomeController.rb
@@ -2,4 +2,16 @@ class HomeController < ApplicationController
 	get '/' do
     erb :index
 	end
+
+	get '/one' do
+		erb :one
+	end
+
+	get '/two' do
+		erb :two
+	end
+
+	get '/three' do
+		erb :three
+	end
 end
